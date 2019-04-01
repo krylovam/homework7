8c869778fd27885cff40fd660f872c9881e2f285
Krylova Maria <krylova.maria.2629@gmail.com>
2019-04-01 22:59:59 +0300

    4

--- src/test/java/Test3.java
+++ src/test/java/Test3.java
@@ -7,24 +7,20 @@ import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Test3 extends BaseRunner {
     @Test
     public void test3(){
-        System.setProperty("webdriver.chrome.driver", "C:/chromedriver/chromedriver.exe");
-        String downloadFilepath = "D:/MyDeskDownload";
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-        chromePrefs.put("pdfjs.disabled", true);
+
         ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("prefs", chromePrefs);
-        DesiredCapabilities cap = DesiredCapabilities.chrome();
-        cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-        cap.setCapability(ChromeOptions.CAPABILITY, options);
-        //WebDriver driver = new ChromeDriver(cap);
-        //driver.get("chrome://settings/content/pdfDocuments");
-       // new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='control'][@role='button']")));
-        //driver.findElement(By.x("//*[@id='control'][@role='button'][@aria-label=\'Скачивать файлы PDF, а не автоматически открывать их в Chrome']")).click();
-        //clickXpath(driver,"//cr-toggle[@id='control']");
+
+        options.addArguments("--disable-notifications");
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("plugin s.always_open_pdf_externally", true);
+        prefs.put("download.default_directory",System.getProperty("user.dir"));
+        options.setExperimentalOption("prefs",prefs);
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.tinkoff.ru/mobile-operator/documents/");
         clickXpath(driver,"//a[contains(text(),'Описание акции «Первый месяц бесплатно»')]");
             }
